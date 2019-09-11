99fb0ef7a32f5a4b407cbf821d1fe8751a16776d
electronstudio <github@electronstudio.co.uk>
2019-09-11 16:37:10 +0100

    some renaming

--- examples/rl_keyboard_input.py
+++ examples/rl_keyboard_input.py
@@ -1,5 +1,4 @@
 from richlib import *
-
 alien = Actor('trooper')
 alien.size = (20,20,20)
 alien.pos = (0, 10, 10)
@@ -8,7 +7,6 @@ def draw():
     clear()
     alien.draw()
 
-
 def update():
     if keyboard.right:
         alien.x = alien.x + 1
@@ -16,7 +14,6 @@ def update():
         alien.x = alien.x - 1
 
 run()
-
 
 """TODO
     make the alien move up and down as well as left and right
--- examples/rl_sounds.py
+++ examples/rl_sounds.py
@@ -3,7 +3,6 @@
     Click the mouse to play the sound.
 """
 from richlib import *
-
 sound = Sound('eep')
 sound.volume = 0.7
 sound.pitch = 0.5
@@ -11,13 +10,10 @@ sound.pitch = 0.5
 def draw():
     clear()
 
-
 def update():
     if mouse.clicked:
         sound.play()
-
 run()
-
 """TODO
     Change the pitch each time the mouse is clicked.
     Edit program 17 so that it plays a sound when you hit the cube.
